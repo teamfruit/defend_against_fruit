a0664171c9cdaf4f2d42e6faeba457c0961ba198
Matthew Tardiff <mattrix@gmail.com>
2013-06-13 14:35:01 -0500

    Moved integration tests to be part of the standard_sdist_run (required an update of fruit-dist dependencies to 1.2.79).

--- pypi_redirect/ci_config/ci_run.py
+++ pypi_redirect/ci_config/ci_run.py
@@ -5,12 +5,32 @@ from fruit_dist.ci_utils import standard_sdist_run
 from fruit_dist.exec_utils import install_dev
 
 
-def run_integration_tests():
+def _path_from_here(*path):
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    test_dir = os.path.abspath(os.path.join(
-        this_dir, '..', 'pypi_redirect_integration', 'tests'))
-    os.chdir(test_dir)
-    subprocess.check_call(['nosetests'])
+    rel_path = os.path.join(this_dir, *path)
+    abs_path = os.path.abspath(rel_path)
+    return abs_path
+
+
+def _cwd_as(path):
+    class Context(object):
+        def __enter__(self):
+            self.old_cwd = os.getcwd()
+            os.chdir(path)
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            os.chdir(self.old_cwd)
+
+    return Context()
+
+
+def _run_integration_tests():
+    install_dev()
+
+    test_dir = _path_from_here('..', 'pypi_redirect_integration', 'tests')
+
+    with _cwd_as(test_dir):
+        subprocess.check_call(['nosetests'])
 
 
 if __name__ == '__main__':
@@ -18,6 +38,4 @@ if __name__ == '__main__':
         level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s')
 
-    standard_sdist_run()
-    install_dev()
-    run_integration_tests()
+    standard_sdist_run(integration_tests_fn=_run_integration_tests)
