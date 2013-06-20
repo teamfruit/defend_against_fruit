ddb1c19fd4563475da7add262441cce1dec02779
Matthew Tardiff <mattrix@gmail.com>
2013-06-20 17:51:24 -0500

    Fixed link in setup.py and added some important missing fields.

--- pypi_redirect/setup.py
+++ pypi_redirect/setup.py
@@ -9,16 +9,18 @@ except ImportError:
 setup(
     name='pypi_redirect',
     version=open('version.txt').read().strip(),
-    url='https://github.com/teamfruit/fruit_dist',
+    url='http://teamfruit.github.io/defend_against_fruit/',
     author='Team Fruit',
     author_email='defend.against.fruit@gmail.com',
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "*.test", "*.test.*"]),
     license="Apache License 2.0",
+    description='Treat pypi.python.org as a remote repo in Artifactory',
     long_description=open('README.txt').read(),
     test_suite='nose.collector',
     install_requires=['lxml', 'requests', 'CherryPy>=3.1', 'pywin32>=217'],
     tests_require=['lxml', 'nose>=1.2.1'],
+    keywords="pypi artifactory",
     data_files=[
         ('pypi_redirect/config',
          ['server_config/pypi_redirect.conf.template'])],
