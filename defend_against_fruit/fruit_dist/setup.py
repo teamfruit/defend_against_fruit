614c9651f8cb494142f695d70d0a5d35ab78f132
Matthew Tardiff <mattrix@gmail.com>
2013-06-20 18:10:31 -0500

    Fixed URL in setup.py meta-data and added description and keywords.

--- defend_against_fruit/fruit_dist/setup.py
+++ defend_against_fruit/fruit_dist/setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-# import sys
-# sys.path.append(r'C:\path\to\pydev\egg')
-# import pydevd
-# pydevd.settrace('localhost', port=5555, stdoutToServer=True, stderrToServer=True)
-
 # Work-around for a bug: http://bugs.python.org/msg170215
 try:
     import multiprocessing
@@ -15,18 +10,23 @@ setup(
     #name attribute is now being used by setuptools_webdav
     name='fruit_dist',
     version=open('version.txt').read().strip(),
-    url='https://github.com/teamfruit/fruit_dist',
+    url='http://teamfruit.github.io/defend_against_fruit/',
     author='Team Fruit',
     author_email='defend.against.fruit@gmail.com',
     packages=find_packages(exclude=["*.tests", "*.tests.*"]),
     license='Apache License 2.0',
+    description='Provides various utilities for setup utils and continuous '
+                'integration',
     long_description=open('README.rst').read(),
     test_suite='nose.collector',
+    keywords="pypi artifactory continuous integration deployment ci cd",
     entry_points={
         "distutils.commands": [
-            "artifactory_upload = fruit_dist.commands.artifactory_upload:artifactory_upload",
+            "artifactory_upload = fruit_dist.commands.artifactory_upload:"
+            "artifactory_upload",
             "freeze = fruit_dist.commands.freeze:freeze",
-            "module_generator = fruit_dist.commands.module_generator_command:ModuleGeneratorCommand",
+            "module_generator = fruit_dist.commands.module_generator_command:"
+            "ModuleGeneratorCommand",
             ]},
     setup_requires=['nose>=1.2.1'],
     install_requires=['requests>=1.1.0', 'pkginfo>=1.0b2'],
--- python_garden/fruit/citrus/setup.py
+++ python_garden/fruit/citrus/setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
 
-# import sys
-# sys.path.append(r'C:\path\to\pydev\egg')
-# import pydevd
-# pydevd.settrace('localhost', port=5555, stdoutToServer=True, stderrToServer=True)
-
 # Work-around for a bug: http://bugs.python.org/msg170215
 try:
     import multiprocessing
@@ -15,13 +10,16 @@ setup(
     #name attribute is now being used by setuptools_webdav
     name='citrus',
     version=open('version.txt').read().strip(),
-    url='https://github.com/teamfruit/fruit_dist',
+    url='http://teamfruit.github.io/defend_against_fruit/',
     author='Team Fruit',
     author_email='defend.against.fruit@gmail.com',
     packages=find_packages(exclude=["*.tests", "*.tests.*"]),
     license="Apache License 2.0",
+    description='Multi-module continuous deployment example using '
+                'Defend Against Fruit',
     long_description=open('README.txt').read(),
     test_suite='nose.collector',
+    keywords="pypi artifactory continuous integration deployment ci cd",
     install_requires=['pest>=1.1.47'],
     setup_requires=['nose>=1.2.1'],
 )
